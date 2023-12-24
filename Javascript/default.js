e8a71f5805e9d312fe6f77a979f819ef4158e926
hyosang <hyomee@naver.com>
2023-12-25 02:14:16 +0900

    커밋

--- Javascript/default.js
+++ Javascript/default.js
@@ -3,7 +3,7 @@ import {user} from './data.js';
 const helloMessage = "안녕하세요";
 user.name = "김길동"
 
-export function fnHelloMsg (name) {
+export function fnHelloMsg () {
     return `${user.name}님 ${helloMessage}`;
 }
  
--- Javascript/named.js
+++ Javascript/named.js
@@ -1,7 +1,7 @@
 import {user} from './data.js';
 
 export const helloMessage = "안녕하세요";
-export const fnHello = ( message) => {     
+export const fnHello = (message) => {     
     return `${user.name}님 ${message}`;
 }
 
