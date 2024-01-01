1c47af061a03d61714ed8fdc53b6475032398b9c
hyosang <hyomee@naver.com>
2024-01-02 01:37:40 +0900

    0101

--- project/vue-first-app/src/components/ForifTempCom.vue
+++ project/vue-first-app/src/components/ForifTempCom.vue
@@ -1,21 +1,78 @@
+<script setup>
+    import {computed, reactive, ref } from 'vue';
+    import { sortBy  } from 'lodash';
+
+    const users = [
+                { name: '하나', age: 33 },
+                { name: '둘', age: 33 },
+                { name: '셋', age: 17 },
+                { name: '넷', age: 16 }
+                ] ;
+
+    const fnLegalUsersComputed= computed(() => { 
+        return users.filter(user => user.age >= 18)
+    }); 
+
+    const fnLegalUsers = () => { 
+        console.log(users);
+        return users.filter(user => user.age >= 18)
+    }; 
+
+    const fnSortUsers = () => { 
+        console.log(users);
+        return sortBy(users, ['age'])
+    };
+ 
+</script>
+<!-- 
 <script>
+    import {computed, reactive, ref } from 'vue';
+
+    const users = [
+                    { name: '하나', age: 33 },
+                    { name: '둘', age: 33 },
+                    { name: '셋', age: 17 },
+                    { name: '넷', age: 16 }
+                ];
+
+    const fnUsers = () => {
+        const fnLegalUsersCompted = computed(() => { 
+                console.log("setup() : fnLegalUsersCompted",  users);
+                return  users.filter(user => user.age >= 18)
+            }); 
+
+        const fnLegalUsers = () => { 
+            console.log("setup() : fnLegalUsers",  users);
+            return users.filter(user => user.age >= 18)
+        }; 
+
+        return {
+            fnLegalUsersCompted, 
+            fnLegalUsers
+        }
+    }
+
     export default {
         data() {
             return {
-                users: [
-                { name: '홍일이', age: 33 },
-                { name: '홍이이', age: 33 },
-                { name: '홍삼이', age: 17 },
-                { name: '홍사이', age: 16 }
-                ]
+                users
+            }
+        },  
+        setup() { 
+            const {fnLegalUsersCompted, fnLegalUsers } = fnUsers();
+            return {
+                fnLegalUsersCompted,
+                fnLegalUsers,
             }
         }
+         
     }
-</script>
+</script>  -->
 
+ 
 <template>
-    <template v-for="user in users" :key="user.name">
-      <p v-if="user.age >= 18">{{ user.name }}</p>
+    <template v-for="user in fnSortUsers()" :key="user.name"> 
+        {{ user.name }} {{ user.age }}  <br/>
     </template>
 </template>
   
